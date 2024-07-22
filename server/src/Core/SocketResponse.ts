7e8c1f813457ada313748c29d4994c53f449eccf
in4in <durashka@mail.ru>
2024-07-23 03:27:59 +0700

    only singlplayer

--- server/src/Core/SocketResponse.ts
+++
@@ -1,4 +0,0 @@
-export default interface SocketResponse
-{
-	toSocketJson() : any;
-}
--- server/src/index.ts
+++
@@ -1,8 +0,0 @@
-import GameServer from "./Core/GameServer";
-
-let port = 5174;
-let game = new GameServer(port);
-
-game.start();
-
-console.log('Start on port', port);
