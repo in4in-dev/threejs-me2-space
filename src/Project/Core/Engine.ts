3ac4ac340911077a975ae7b2ad7fe096a39aed3c
in4in <durashka@mail.ru>
2025-05-04 11:35:17 +0400

    { antialias: true }

--- src/Project/Core/Engine.ts
+++ src/Project/Core/Engine.ts
@@ -34,7 +34,7 @@ export default abstract class Engine
 
 		let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-		let renderer = new THREE.WebGLRenderer();
+		let renderer = new THREE.WebGLRenderer({ antialias: true });
 		renderer.setSize(window.innerWidth, window.innerHeight);
 		renderer.setPixelRatio(window.devicePixelRatio);
 		element.appendChild(renderer.domElement);
